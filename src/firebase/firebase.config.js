d0a1e7733392a1d57803accfa5ac32488b435fa8
Safayat-Hasan <safayathasananim20@gmail.com>
2023-10-07 21:50:52 +0600

    initialized the assignment through installing the reqired plugins

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVRUGzfOxHspeFaZbHQUgSXs3cPBs7rOQ",
+  authDomain: "react-event-management-4137e.firebaseapp.com",
+  projectId: "react-event-management-4137e",
+  storageBucket: "react-event-management-4137e.appspot.com",
+  messagingSenderId: "989872563690",
+  appId: "1:989872563690:web:2b7e0fbc9d0514a7087e93"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
